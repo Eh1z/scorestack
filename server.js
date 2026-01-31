da9bae05712617da3ea5d012d78bc9df6d031679
Eh1z <godsentehiz11@gmail.com>
2026-01-31 14:49:12 +0100

    Add initial project files including .gitignore, package.json, and server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+import {WebSocketServer, WebSocket} from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+
+
+// Connection established
+wss.on('connection', (socket, request) => {
+    const ip = request.remoteAddress;
+
+    socket.on('message', (rawData) => {
+        console.log(`Received message from ${ip}: ${rawData}`);
+        const message = rawData.toString()
+
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN) {
+                client.send(`Server Broadcast: ${message}`);
+            }
+        })
+    })
+
+
+    socket.on('error', (error) => {
+        console.error(`Error on connection with ${ip}:`, error.message);
+    })
+
+
+    socket.on('close', (code, reason) => {
+        console.log(`Connection with ${ip} closed: ${code} - ${reason}`);
+    })
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
