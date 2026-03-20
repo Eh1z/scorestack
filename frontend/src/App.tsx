f3f7cd58e5fbd687bba8978103c7ab84fb7ad2b5
Eh1z <godsentehiz11@gmail.com>
2026-03-20 17:21:28 +0100

    fix: adjust font size for Scorestack Frontend heading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-7xl'>Scorestack Frontend</div>
+    <div className='text-5xl'>Scorestack Frontend</div>
   )
 }
 
